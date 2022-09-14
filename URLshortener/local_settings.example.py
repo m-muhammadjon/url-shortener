1a1b7ca75c157de013007d751b808b6f13d73d50
Mukhammadjon <muhammadjon.200415@gmail.com>
2022-09-14 19:03:02 +0500

    local_settings.example.py added

---
+++ URLshortener/local_settings.example.py
@@ -0,0 +1,20 @@
+HOST = 'localhost:8000'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'DB_NAME',
+#         'USER': 'DB_USER',
+#         'PASSWORD': 'DB_PASSWORD',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
+# sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3'
+#     }
+# }
