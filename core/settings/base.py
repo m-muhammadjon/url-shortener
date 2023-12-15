2c5ae23b1e8199299fb7102d13ee995b921efc34
Muhammadjon Muxamedov <muhammadjon.200415@gmail.com>
2023-12-15 15:55:10 +0500

    upgraded django version to 5.0

--- core/settings/base.py
+++ core/settings/base.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths 3inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # READING ENV
 env = environ.Env()
